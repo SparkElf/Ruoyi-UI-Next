a98204b773f3244b0be292be17fe43ae6509e2f7
SparkElf <374656045@qq.com>
2024-03-14 11:48:50 +0800

    init:优化了不必要的插件，仅保留自动导入，从vue3官方模板开始搭建，引入ts和devtool

---
+++ vite.config.ts
@@ -0,0 +1,81 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+
+import vue from '@vitejs/plugin-vue'
+
+import AutoImport from 'unplugin-auto-import/vite'
+
+import VueDevTools from 'vite-plugin-vue-devtools'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_ENV } = env
+  return {
+    // 部署生产环境和开发环境下的URL。
+    // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
+    // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
+    base: VITE_APP_ENV === 'production' ? '/' : '/',
+    plugins: [[
+      vue(),
+      VueDevTools(),
+      //自动按需导入依赖
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia'
+        ],
+        dts: true
+      }),
+      //将assets下的icon路径转换为类名直接引用
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
+            symbolId: 'icon-[dir]-[name]',
+            svgoOptions: VITE_APP_ENV === 'production'
+        })
+    ],],
+    resolve: {
+      // https://cn.vitejs.dev/config/#resolve-alias
+      alias: {
+        // 设置路径
+        '~': fileURLToPath(new URL('./', import.meta.url)),
+        // 设置别名
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+      // https://cn.vitejs.dev/config/#resolve-extensions
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    // vite 相关配置
+    server: {
+      port: 80,
+      host: true,
+      open: true,
+      proxy: {
+        // https://cn.vitejs.dev/config/#server-proxy
+        '/dev-api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/dev-api/, '')
+        }
+      }
+    }
+  }
+})
+
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     VueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
---
+++ vite/plugins/index.ts
@@ -0,0 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+
+import createAutoImport from './auto-import'
+import createSvgIcon from './svg-icon'
+
+import createSetupExtend from './setup-extend'
+import VueDevTools from 'vite-plugin-vue-devtools'
+export default function createVitePlugins(viteEnv, isBuild = false) {
+    const vitePlugins = [vue(), VueDevTools()]
+    vitePlugins.push()
+	vitePlugins.push(createSetupExtend())
+    vitePlugins.push(createSvgIcon(isBuild))
+    return vitePlugins
+}
