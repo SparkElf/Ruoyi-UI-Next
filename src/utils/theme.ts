071f201d336839095b9e8752fcc37ec4397ae689
SparkElf <374656045@qq.com>
2024-03-15 11:54:40 +0800

    处理utils/theme类型

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,5 @@
 // 处理主题样式
-export function handleThemeStyle(theme) {
+export function handleThemeStyle(theme: string) {
 	document.documentElement.style.setProperty('--el-color-primary', theme)
 	for (let i = 1; i <= 9; i++) {
 		document.documentElement.style.setProperty(`--el-color-primary-light-${i}`, `${getLightColor(theme, i / 10)}`)
@@ -10,17 +10,13 @@ export function handleThemeStyle(theme) {
 }
 
 // hex颜色转rgb颜色
-export function hexToRgb(str) {
+export function hexToRgb(str: string) {
 	str = str.replace('#', '')
-	let hexs = str.match(/../g)
-	for (let i = 0; i < 3; i++) {
-		hexs[i] = parseInt(hexs[i], 16)
-	}
-	return hexs
+	return str.match(/../g)!.map(s=>parseInt(s, 16))
 }
 
 // rgb颜色转Hex颜色
-export function rgbToHex(r, g, b) {
+export function rgbToHex(r: { toString: (arg0: number) => any }, g: { toString: (arg0: number) => any }, b: { toString: (arg0: number) => any }) {
 	let hexs = [r.toString(16), g.toString(16), b.toString(16)]
 	for (let i = 0; i < 3; i++) {
 		if (hexs[i].length == 1) {
@@ -31,7 +27,7 @@ export function rgbToHex(r, g, b) {
 }
 
 // 变浅颜色值
-export function getLightColor(color, level) {
+export function getLightColor(color: string , level: number) {
 	let rgb = hexToRgb(color)
 	for (let i = 0; i < 3; i++) {
 		rgb[i] = Math.floor((255 - rgb[i]) * level + rgb[i])
@@ -40,7 +36,7 @@ export function getLightColor(color, level) {
 }
 
 // 变深颜色值
-export function getDarkColor(color, level) {
+export function getDarkColor(color: string, level: number) {
 	let rgb = hexToRgb(color)
 	for (let i = 0; i < 3; i++) {
 		rgb[i] = Math.floor(rgb[i] * (1 - level))
