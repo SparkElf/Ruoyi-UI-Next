8d6246768efaa9a2cedcb43dd2fbdc34b26201e6
SparkElf <374656045@qq.com>
2024-03-17 01:24:18 +0800

    修复plugins/auths.ts类型

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -1,6 +1,6 @@
 import useUserStore from '@/store/modules/user'
 
-function authPermission(permission) {
+function authPermission(permission: string) {
   const all_permission = "*:*:*";
   const permissions = useUserStore().permissions
   if (permission && permission.length > 0) {
@@ -12,7 +12,7 @@ function authPermission(permission) {
   }
 }
 
-function authRole(role) {
+function authRole(role: string) {
   const super_admin = "admin";
   const roles = useUserStore().roles
   if (role && role.length > 0) {
@@ -26,34 +26,34 @@ function authRole(role) {
 
 export default {
   // 验证用户是否具备某权限
-  hasPermi(permission) {
+  hasPermi(permission: string) {
     return authPermission(permission);
   },
   // 验证用户是否含有指定权限，只需包含其中一个
-  hasPermiOr(permissions) {
-    return permissions.some(item => {
+  hasPermiOr(permissions: string[]) {
+    return permissions.some((item: any) => {
       return authPermission(item)
     })
   },
   // 验证用户是否含有指定权限，必须全部拥有
-  hasPermiAnd(permissions) {
-    return permissions.every(item => {
+  hasPermiAnd(permissions: string[]) {
+    return permissions.every((item: any) => {
       return authPermission(item)
     })
   },
   // 验证用户是否具备某角色
-  hasRole(role) {
+  hasRole(role: string) {
     return authRole(role);
   },
   // 验证用户是否含有指定角色，只需包含其中一个
-  hasRoleOr(roles) {
-    return roles.some(item => {
+  hasRoleOr(roles: string[]) {
+    return roles.some((item: any) => {
       return authRole(item)
     })
   },
   // 验证用户是否含有指定角色，必须全部拥有
-  hasRoleAnd(roles) {
-    return roles.every(item => {
+  hasRoleAnd(roles: string[]) {
+    return roles.every((item: any) => {
       return authRole(item)
     })
   }
