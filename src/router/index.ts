18d71859114529212e7be9be11e489d0c18e9e59
SparkElf <374656045@qq.com>
2024-03-26 11:50:15 +0800

    恢复动态路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -84,76 +84,47 @@ export const constantRoutes = [
 
 // 动态路由，基于用户权限动态去加载
 export const dynamicRoutes = [
-  // {
-  //   path: '/system/user-auth',
-  //   component: Layout,
-  //   hidden: true,
-  //   permissions: ['system:user:edit'],
-  //   children: [
-  //     {
-  //       path: 'role/:userId(\\d+)',
-  //       component: () => import('@/views/system/user/authRole.vue'),
-  //       name: 'AuthRole',
-  //       meta: { title: '分配角色', activeMenu: '/system/user' }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/system/role-auth',
-  //   component: Layout,
-  //   hidden: true,
-  //   permissions: ['system:role:edit'],
-  //   children: [
-  //     {
-  //       path: 'user/:roleId(\\d+)',
-  //       component: () => import('@/views/system/role/authUser.vue'),
-  //       name: 'AuthUser',
-  //       meta: { title: '分配用户', activeMenu: '/system/role' }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/system/dict-data',
-  //   component: Layout,
-  //   hidden: true,
-  //   permissions: ['system:dict:list'],
-  //   children: [
-  //     {
-  //       path: 'index/:dictId(\\d+)',
-  //       component: () => import('@/views/system/dict/data.vue'),
-  //       name: 'Data',
-  //       meta: { title: '字典数据', activeMenu: '/system/dict' }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/monitor/job-log',
-  //   component: Layout,
-  //   hidden: true,
-  //   permissions: ['monitor:job:list'],
-  //   children: [
-  //     {
-  //       path: 'index/:jobId(\\d+)',
-  //       component: () => import('@/views/monitor/job/log.vue'),
-  //       name: 'JobLog',
-  //       meta: { title: '调度日志', activeMenu: '/monitor/job' }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/tool/gen-edit',
-  //   component: Layout,
-  //   hidden: true,
-  //   permissions: ['tool:gen:edit'],
-  //   children: [
-  //     {
-  //       path: 'index/:tableId(\\d+)',
-  //       component: () => import('@/views/tool/gen/editTable.vue'),
-  //       name: 'GenEdit',
-  //       meta: { title: '修改生成配置', activeMenu: '/tool/gen' }
-  //     }
-  //   ]
-  // }
+
+  {
+    path: '/system/user-auth/role/:userId(\\d+)',
+    component: () => import('@/views/system/user/authRole.vue'),
+    hidden: true,
+    permissions: ['system:user:edit'],
+    meta: { title: '分配角色', activeMenu: '/system/user' },
+    name: 'AuthRole',
+  },
+  {
+    path: '/system/role-auth/user/:roleId(\\d+)',
+    component: () => import('@/views/system/role/authUser.vue'),
+    hidden: true,
+    permissions: ['system:role:edit'],
+    name: 'AuthUser',
+    meta: { title: '分配用户', activeMenu: '/system/role' }
+  },
+  {
+    path: '/system/dict-data/index/:dictId(\\d+)',
+    component: () => import('@/views/system/dict/data.vue'),
+    name: 'Data',
+    meta: { title: '字典数据', activeMenu: '/system/dict' },
+    hidden: true,
+    permissions: ['system:dict:list'],
+  },
+  {
+    path: '/monitor/job-log/index/:jobId(\\d+)',
+    component: () => import('@/views/monitor/job/log.vue'),
+    hidden: true,
+    permissions: ['monitor:job:list'],
+    name: 'JobLog',
+    meta: { title: '调度日志', activeMenu: '/monitor/job' }
+  },
+  {
+    path: '/tool/gen-edit/index/:tableId(\\d+)',
+    component: () => import('@/views/tool/gen/editTable.vue'),
+    name: 'GenEdit',
+    meta: { title: '修改生成配置', activeMenu: '/tool/gen' },
+    hidden: true,
+    permissions: ['tool:gen:edit'],
+  }
 ] as Route[]
 
 const router = createRouter({
